a044c1418323b006c6b1f7a79ddca1fa794b5237
Andy Davidoff <github@andy.disruptek.com>
2021-07-10 13:07:37 -0400

    just force it maybe

--- tests/setup.sh
+++ tests/setup.sh
@@ -1,6 +1,6 @@
 # This is a library intended to be invoked as ". ${0%script}cd.sh".
 # It sets up $d and cd's into a data directory or dies trying.
-: ${d:="$TMP/d"} # or maybe /dev/shm/data or something
+: ${d:="$TMPDIR/d"} # or maybe /dev/shm/data or something
 : ${nio:="nio"}      # Use installed `nio`; Could be, e.g. "nim r path/nio.nim"
 if [ -r "$d" -a -w "$d" -a -x "$d" -a -d "$d" ]
 then
